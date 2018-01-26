22220c96ed49cacde015f522c3545ba681cb3b3f
chaoxin <chaoxin13@hotmail.com>
2018-01-26 22:52:01 +0100

    Added : Object pool script , Wave Spawning system , 2 formation in the resources folder. Resource folder , scene folder, script folder. Wavespawntest scene

---
+++ GameJam2018/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Formation[] availableFormations;
+    public int[] FormationSpawnOrder;
+    public int currentWave, formationsInWave;
+    public float interVal, interValDecrease,interValLimit, waveSpawnInterVal, WaveSpawnInterValDecrease,waveSpawnInterValLimit;
+   
+    public float InterVals { get {return interVal; }set { if (interVal < interValLimit) { interVal = interValLimit; } interVal = value; }}
+    public float WaveSpawnInterVal { get { return waveSpawnInterVal; } set
+        {
+            if (waveSpawnInterVal < waveSpawnInterValLimit) { waveSpawnInterVal = waveSpawnInterValLimit; }
+            waveSpawnInterVal = value;
+        }
+        }
+
+    public IEnumerator loopingThroughFormationList() {
+
+        for (int i = 0; i < formationsInWave; i++)
+        {   
+            yield return new WaitForSeconds(waveSpawnInterVal);
+            InterVals -= interValDecrease;
+            GameObject formation = ObjectPool.objectPool.GetPooledObjct(availableFormations[FormationSpawnOrder[currentWave]].waveToSpawn, true);
+            formation.transform.position = availableFormations[FormationSpawnOrder[currentWave]].waveSpawnPoint;
+            formation.SetActive(true);
+            if(currentWave < FormationSpawnOrder.Length)
+            currentWave++;
+        }
+
+        if (currentWave == FormationSpawnOrder.Length / 3) { 
+            formationsInWave++;
+        }
+        WaveSpawnInterVal -= WaveSpawnInterValDecrease;
+        yield return new WaitForSeconds(interVal);
+        InterVals -= interValDecrease;
+        StartCoroutine(loopingThroughFormationList());
+    }
+
+	void Start () {
+        StartCoroutine(loopingThroughFormationList());
+	}
+	
+}
+
+[System.Serializable]
+public class Formation
+{   
+    
+    //public int waveNr;
+    public string waveToSpawn;
+    public Vector2 waveSpawnPoint;
+}
+
