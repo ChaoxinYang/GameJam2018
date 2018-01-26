b8fb160068062ebdb78404fad49e4792839d7c72
Scott <schepers.scott@hotmail.com>
2018-01-26 21:16:07 +0100

    Added: clamps player 1 rotation

--- GameJam2018/Assets/playerMovement.cs
+++ GameJam2018/Assets/playerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour {
 
     public float rotPlayer1 = 90f;
+    public float rotationSpeed = 1f;
     // Use this for initialization
     void Start () {
 		
@@ -12,9 +13,10 @@ public class playerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if (Input.GetKey(KeyCode.A)) rotPlayer1++;
-        if (Input.GetKey(KeyCode.D)) rotPlayer1--;
-        
+        if (Input.GetKey(KeyCode.A)) rotPlayer1+= rotationSpeed;
+        if (Input.GetKey(KeyCode.D)) rotPlayer1-= rotationSpeed;
+
+        rotPlayer1 = Mathf.Clamp(rotPlayer1, -90f, 90f);
         transform.eulerAngles = new Vector3(0f, 0f, rotPlayer1);
         transform.position = new Vector3(0f, 0f, 0f);
         transform.Translate(new Vector3(-1.3f, 0f, 0f));
