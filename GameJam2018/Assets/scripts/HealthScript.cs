b33a7e19864e2ea2807fc46b219ecc3ad1005dae
Max Weijers <maxweijers@hotmail.nl>
2018-01-26 20:41:10 +0100

    Health + Score script added, can be used in other scripts, + particles for space

---
+++ GameJam2018/Assets/scripts/HealthScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour {
+	public int health = 500;
+
+	private Text myText;
+	// Use this for initialization
+	void Start () {
+		myText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(health <= 0){
+			string currentScene = SceneManager.GetActiveScene().name;
+			SceneManager.LoadScene(currentScene);
+			ScoreKeeper.Reset();
+		}
+		if(Input.GetKeyDown(KeyCode.F)){
+			Damage(100);
+		}
+	}
+
+	public void Damage(int hitPoints){
+		health -= hitPoints;
+		myText.text = health.ToString();
+	}
+}
---
+++ GameJam2018/Assets/scripts/ScoreKeeper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour {
+	public static int score;
+
+	private Text myText;
+	// Use this for initialization
+	void Start () {
+		myText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space")){
+			Score(50);
+		}
+	}
+	public void Score(int pointsGained){
+		score += pointsGained;
+		myText.text = score.ToString();
+	}
+
+	public static void Reset(){
+		score = 0;
+	}
+}
