db14fdacc8e2a4c33b83e6d5c161a9a6eecde9e3
Brian Duncker <luguoslight@gmail.com>
2018-01-26 21:01:54 +0100

    Plays a moving animation of the weapons when the S or Down Arrow key is pressed, weapon states are not being changed yet.

---
+++ GameJam2018/Assets/Scripts/WeaponSwitch.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour {
+    private bool isSwitching, doneSwitching;
+    private Vector3 leftPosition, rightPosition;
+    public GameObject leftWeapon, rightWeapon;
+
+	void Start () {
+        leftPosition = leftWeapon.transform.position;
+        rightPosition = rightWeapon.transform.position;
+        isSwitching = false;
+        doneSwitching = false;
+	}
+
+	void Update () {
+		if ((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && isSwitching == false)
+        {
+            isSwitching = true;
+        }
+
+        if (isSwitching == true)
+        {
+            if (doneSwitching == false)
+            {
+                leftWeapon.transform.position = Vector3.MoveTowards(leftWeapon.transform.position, transform.position, Time.deltaTime);
+                rightWeapon.transform.position = Vector3.MoveTowards(rightWeapon.transform.position, transform.position, Time.deltaTime);
+            }
+            else
+            {
+                leftWeapon.transform.position = Vector3.MoveTowards(leftWeapon.transform.position, leftPosition, Time.deltaTime);
+                rightWeapon.transform.position = Vector3.MoveTowards(rightWeapon.transform.position, rightPosition, Time.deltaTime);
+            }
+        }
+
+        if (leftWeapon.transform.position == transform.position)
+        {
+            doneSwitching = true;
+        }
+
+        if (leftWeapon.transform.position == leftPosition && doneSwitching == true)
+        {
+            isSwitching = false;
+            doneSwitching = false;
+        }
+	}
+}
