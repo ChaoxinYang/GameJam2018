1cb5ace7ca2cd5c7130f898ffd1a4e05dfafc68a
Max Weijers <maxweijers@hotmail.nl>
2018-01-27 01:42:39 +0100

    Updated collisions, score and health + timer works

--- GameJam2018/Assets/scripts/Collisions/EnemyCollision.cs
+++ GameJam2018/Assets/scripts/Collisions/EnemyCollision.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class EnemyCollision : MonoBehaviour {
 	private HealthScript healthScript;
+
+	public float enemyHealth = 10f;
+
+	private ScoreKeeper scoreKeeper;
 	// Use this for initialization
 	void Start () {
 		healthScript = GameObject.Find("Health").GetComponent<HealthScript>();
+		scoreKeeper = GameObject.Find("Score").GetComponent<ScoreKeeper>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(enemyHealth <= 0){
+			gameObject.SetActive(false);
+			scoreKeeper.Score(50);
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D coll){
-		gameObject.SetActive(false);
-		healthScript.Damage(100);
+		//gameObject.SetActive(false);
+		if(coll.gameObject.CompareTag("Bullet")){
+			enemyHealth -= 5;
+		}else{
+			healthScript.Damage(100);
+			gameObject.SetActive(false);
+		}
 	}
 }
