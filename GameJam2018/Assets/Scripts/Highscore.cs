25151e56c7118dcd75ca709a4dc691e5dde2eff3
Brian Duncker <luguoslight@gmail.com>
2018-01-27 21:40:11 +0100

    Players are now able to input names and are added to the highscore list with their score, which is then read.

---
+++ GameJam2018/Assets/Scripts/Highscore.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class Highscore : MonoBehaviour {
+    public GameObject highscoreList, enterHighscore;
+    private Text highscoreListText, enterHighscoreScore;
+
+    void Start () {
+        highscoreListText = highscoreList.transform.Find("Scores").GetComponent<Text>();
+        enterHighscoreScore = enterHighscore.transform.Find("Score").GetComponent<Text>();
+        highscoreListText.text = null;
+        enterHighscoreScore.text = ScoreKeeper.score.ToString();
+        UpdateHighscore();
+        highscoreList.SetActive(false);
+	}
+
+    public void WriteHighscore()
+    {
+        int oldScore, newScore;
+        string oldName, newName;
+        newScore = ScoreKeeper.score;
+        Debug.Log("test");
+        newName = enterHighscore.transform.Find("PlayerOneInput").GetComponentInChildren<Text>().text + " & " + enterHighscore.transform.Find("PlayerTwoInput").GetComponentInChildren<Text>().text;
+        Debug.Log("test2");
+        for (int i = 0; i < 10; i++)
+        {
+            if (PlayerPrefs.HasKey("HighscoreScore_" + i.ToString()))
+            {
+                if (PlayerPrefs.GetInt("HighscoreScore_" + i.ToString()) < newScore)
+                {
+                    oldScore = PlayerPrefs.GetInt("HighscoreScore_" + i.ToString());
+                    oldName = PlayerPrefs.GetString("HighscoreName_" + i.ToString());
+                    PlayerPrefs.SetInt("HighscoreScore_" + i.ToString(), newScore);
+                    PlayerPrefs.SetString("HighscoreName_" + i.ToString(), newName);
+                    newScore = oldScore;
+                    newName = oldName;
+                }
+            }
+            else
+            {
+                PlayerPrefs.SetInt("HighscoreScore_" + i.ToString(), newScore);
+                PlayerPrefs.SetString("HighscoreName_" + i.ToString(), newName);
+                newScore = 0;
+                newName = "Player One & Player Two";
+            }
+        }
+        PlayerPrefs.Save();
+        UpdateHighscore();
+        enterHighscore.SetActive(false);
+        highscoreList.SetActive(true);
+    }
+
+    void UpdateHighscore()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+
+            if (PlayerPrefs.HasKey("HighscoreScore_" + i.ToString()))
+            {
+                highscoreListText.text += i.ToString("00") + ". " + PlayerPrefs.GetInt("HighscoreScore_" + i.ToString()) + " " + PlayerPrefs.GetString("HighscoreName_" + i.ToString()) + "\n";
+            }
+            else
+            {
+                highscoreListText.text += i.ToString("00") + ". " + "\n";
+            }
+        }
+    }
+}
